c5d843f99c53c6748ce98a019fed75df2f76a999
isadia-brian <developerisadia@gmail.com>
2023-09-11 22:16:34 +0300

    added edit button

--- app/(client)/profile/page.js
+++ app/(client)/profile/page.js
@@ -1,12 +1,6 @@
 "use client";
-import { useEffect, useState } from "react";
-import SplashScreen from "@/app/components/SplashScreen";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { categories, liqours } from "../../../data";
-import HighlightCard from "@/app/components/HighlightCard";
-import CategoryCard from "@/app/components/CategoryCard";
-import PopularCard from "@/app/components/PopularCard";
-import ForYouCard from "@/app/components/ForYouCard";
+import { useState } from "react";
+
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { Avatar } from "antd";
@@ -18,9 +12,14 @@ export default function Profile() {
   return (
     <main className="relative h-full w-full ">
       <div className="relative h-full mt-[70px] px-6 py-8 md:px-12 ">
-        <div className="w-full md:w-[600px] md:mx-auto  my-4 mb-4 md:my-8 flex h-full  items-center space-x-5 border-b-[0.8px] border-slate-400 pb-5">
+        <div className="w-full md:w-[600px] md:mx-auto  mt-4 mb-10 md:my-8 flex h-full  items-center space-x-5 border-b-[0.8px] border-slate-400 pb-5">
           <Avatar style={{ backgroundColor: "white" }} size="large" />
           <p className="font-bold uppercase">{email}</p>
+        </div>
+        <div className="w-full">
+          <button className="w-full py-3 border border-slate-400 shadow-2xl font-light text-sm">
+            Edit Profile
+          </button>
         </div>
       </div>
     </main>
